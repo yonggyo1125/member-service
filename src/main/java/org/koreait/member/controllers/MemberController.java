121d58d745b92e9775623ead20bbf00208d0cb6f
yonggyo1125 <yonggyo00@kakao.com>
2025-01-13 16:57:55 +0900

    .

--- src/main/java/org/koreait/member/controllers/MemberController.java
+++ src/main/java/org/koreait/member/controllers/MemberController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.koreait.global.exceptions.BadRequestException;
 import org.koreait.global.libs.Utils;
 import org.koreait.member.services.MemberUpdateService;
+import org.koreait.member.validators.JoinValidator;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -16,10 +17,13 @@ public class MemberController {
 
     private final Utils utils;
     private final MemberUpdateService updateService;
+    private final JoinValidator joinValidator;
 
     @PostMapping("/join")
     @ResponseStatus(HttpStatus.CREATED)
     public void join(@RequestBody @Valid RequestJoin form, Errors errors) {
+
+        joinValidator.validate(form, errors);
 
         if (errors.hasErrors()) {
             throw new BadRequestException(utils.getErrorMessages(errors));
