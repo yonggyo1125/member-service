333beda95970a2f22b64a4849e8334c7d37c8c39
yonggyo1125 <yonggyo00@kakao.com>
2025-01-14 10:02:19 +0900

    .

--- src/main/java/org/koreait/member/jwt/TokenService.java
+++ src/main/java/org/koreait/member/jwt/TokenService.java
@@ -1,14 +1,14 @@
 package org.koreait.member.jwt;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import org.koreait.global.exceptions.UnAuthorizedException;
+import org.koreait.global.libs.Utils;
 import org.koreait.member.MemberInfo;
 import org.koreait.member.services.MemberInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -31,6 +31,9 @@ public class TokenService {
 
     private final JwtProperties properties;
     private final MemberInfoService infoService;
+
+    @Autowired
+    private Utils utils;
 
     private Key key;
 
@@ -102,5 +105,38 @@ public class TokenService {
         String token = authHeader.substring(7);
 
         return authenticate(token);
+    }
+
+    /**
+     * 토큰 검증
+     *
+     * @param token
+     */
+    public void validate(String token) {
+        String errorCode = null;
+        Exception error = null;
+        try {
+            Jwts.parser().setSigningKey(key).build().parseClaimsJws(token).getPayload();
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            errorCode = "JWT.malformed";
+            error = e;
+        } catch (ExpiredJwtException e) { // 토큰 만료
+            errorCode = "JWT.expired";
+            error = e;
+        } catch (UnsupportedJwtException e) {
+            errorCode = "JWT.unsupported";
+            error = e;
+        } catch (Exception e) {
+            errorCode = "JWT.error";
+            error = e;
+        }
+
+        if (StringUtils.hasText(errorCode)) {
+            throw new UnAuthorizedException(utils.getMessage(errorCode));
+        }
+
+        if (error != null) {
+            error.printStackTrace();
+        }
     }
 }
