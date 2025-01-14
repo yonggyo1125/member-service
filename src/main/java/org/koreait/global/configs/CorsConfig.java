e1589af6387b9a2627d265a458ee97dbf180671a
yonggyo1125 <yonggyo00@kakao.com>
2025-01-14 12:44:07 +0900

    .

--- src/main/java/org/koreait/global/configs/CorsConfig.java
+++ src/main/java/org/koreait/global/configs/CorsConfig.java
@@ -1,16 +1,21 @@
 package org.koreait.global.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
-    //@Value("${cors.allowed}")
-    //private List<String> allowedOrigin;
+    @Value("${cors.allowed}")
+    private String allowedOrigin;
 
     @Bean
     public CorsFilter corsFilter() {
@@ -21,12 +26,13 @@ public class CorsConfig {
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
 
-      //  if (allowedOrigin == null || allowedOrigin.isEmpty()) {
+        if (StringUtils.hasText(allowedOrigin)) {
+            List<String> origins = Arrays.stream(allowedOrigin.split(",")).toList();
+            config.setAllowedOrigins(origins);
+            config.setAllowCredentials(true);
+        } else {
             config.addAllowedOrigin("*");
-       // } else {
-       //     config.setAllowedOrigins(allowedOrigin);
-       //     config.setAllowCredentials(true);
-        //}
+        }
 
         source.registerCorsConfiguration("/**", config);
 
--- src/main/java/org/koreait/member/controllers/MemberController.java
+++ src/main/java/org/koreait/member/controllers/MemberController.java
@@ -1,25 +1,23 @@
 package org.koreait.member.controllers;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.koreait.global.exceptions.BadRequestException;
 import org.koreait.global.libs.Utils;
 import org.koreait.global.rests.JSONData;
+import org.koreait.member.MemberInfo;
 import org.koreait.member.jwt.TokenService;
 import org.koreait.member.services.MemberUpdateService;
 import org.koreait.member.validators.JoinValidator;
 import org.koreait.member.validators.LoginValidator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Member", description = "회원 인증/인가 API")
 @RestController
@@ -70,17 +68,22 @@ public class MemberController {
         if (StringUtils.hasText(frontDomain)) {
             String[] domains = frontDomain.split(",");
             for (String domain : domains) {
-                Cookie cookie = new Cookie("token", token);
-                cookie.setPath("/");
-                cookie.setDomain(domain);
-                cookie.setSecure(true);
-                cookie.setHttpOnly(true);
-                response.addCookie(cookie);
+
+
+                response.setHeader("Set-Cookie", String.format("token=%s; Path=/; Domain=%s; Secure; HttpOnly; SameSite=None", token, domain)); // SameSite: None - 다른 서버에서도 쿠키 설정 가능, Https는 필수
             }
         }
 
         return new JSONData(token);
     }
 
+    /**
+     * 로그인한 회원정보 조회
+     * @return
+     */
+    @GetMapping("/")
+    public JSONData info(@AuthenticationPrincipal MemberInfo memberInfo) {
 
+        return new JSONData(memberInfo.getMember());
+    }
 }
