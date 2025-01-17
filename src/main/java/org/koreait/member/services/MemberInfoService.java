12c49787eefbb6a80b86dbd7490ecbeb1bd3960b
yonggyo1125 <yonggyo00@kakao.com>
2025-01-17 13:20:59 +0900

    .

--- src/main/java/org/koreait/member/services/MemberInfoService.java
+++ src/main/java/org/koreait/member/services/MemberInfoService.java
@@ -15,12 +15,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Lazy
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberInfoService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
